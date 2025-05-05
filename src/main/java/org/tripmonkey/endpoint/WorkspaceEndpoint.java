0acf8e078309f041223938305247bbf14073c0fd
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-05-05 21:17:03 +0100

    GooglePlaces Implementation + Kafka Registry Removal

--- src/main/java/org/tripmonkey/endpoint/WorkspaceEndpoint.java
+++ src/main/java/org/tripmonkey/endpoint/WorkspaceEndpoint.java
@@ -1,10 +1,7 @@
 package org.tripmonkey.endpoint;
 
 import io.quarkus.grpc.GrpcClient;
-import io.smallrye.common.annotation.RunOnVirtualThread;
 import io.smallrye.mutiny.Uni;
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.CookieParam;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.PATCH;
@@ -14,9 +11,8 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import org.jboss.logmanager.Logger;
 import org.tripmonkey.domain.data.User;
-import org.tripmonkey.proto.ProtoSerde;
+import org.tripmonkey.proto.domain.ProtoMapper;
 import org.tripmonkey.rest.domain.WorkspacePatch;
 import org.tripmonkey.rest.patch.Patch;
 import org.tripmonkey.workspace.service.PatchApplier;
@@ -43,7 +39,7 @@ public class WorkspaceEndpoint {
     public Uni<Response> createWorkspace(@CookieParam("user") String user) {
         return Uni.createFrom().optional(User.from(user)).log(String.format("Received request to create workspace for user %s", user))
                 .onItem().ifNull().failWith(() -> new RuntimeException("Invalid UUID for user"))
-                .onItem().ifNotNull().transform(ProtoSerde.userMapper::serialize)
+                .onItem().ifNotNull().transform(ProtoMapper.userMapper::serialize)
                 .onItem().transformToUni(wrkc::create).onItem()
                 .transform(workspaceResponse -> Response.status(200).entity(workspaceResponse.toString()).build())
                 .onFailure().recoverWithItem(throwable -> Response.status(400).entity(throwable.getMessage()).build());
@@ -56,7 +52,7 @@ public class WorkspaceEndpoint {
         return Uni.createFrom().optional(User.from(user)).log(String.format("Received Patch for workspace with id %s", uuid))
                 .onItem().ifNull().failWith(() -> new RuntimeException("Invalid UUID for user"))
                         .onItem().ifNotNull().transform(user1 -> WorkspacePatch.from(uuid, user1.toString(), p))
-                        .onItem().transform(workspacePatch -> ProtoSerde.workspacePatchMapper.serialize(workspacePatch))
+                        .onItem().transform(workspacePatch -> ProtoMapper.workspacePatchMapper.serialize(workspacePatch))
                         .onItem().transformToUni(pac::apply).onItem()
                 .transform(status -> Response.status((int) status.getStatus()).entity(status.getMessage()).build())
                 .onFailure().recoverWithItem(throwable -> Response.status(400).entity(throwable.getMessage()).build());
@@ -69,7 +65,7 @@ public class WorkspaceEndpoint {
         return wrc.fetch(WorkspaceRequest.newBuilder().setWid(uuid).build()).onItem().transform(workspaceResponse -> {
             Response r = Response.status(404).build();
             if(workspaceResponse.hasWorkspace()) {
-                r = Response.accepted().entity(ProtoSerde.workspaceMapper.deserialize(workspaceResponse.getWorkspace())).build();
+                r = Response.accepted().entity(ProtoMapper.workspaceMapper.deserialize(workspaceResponse.getWorkspace())).build();
             }
             return r;
         });
