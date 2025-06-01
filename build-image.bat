ef7fd835b7f3b6ae9638a2b7b19f48fe2278b760
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-06-01 22:25:21 +0100

    New local environments v1: bundled java compilation into docker via multistage docker images, integration with skaffold and preparations for CI/CD

--- build-image.bat
+++ build-image.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 SET previous=%cd%
 CD %~dp0
-CALL mvnw.cmd clean package -Pnative -DskipTests --define quarkus.native.container-build=true --define quarkus.container-image.build=true
+CALL mvnw.cmd clean package -Pnative -DskipTests --define quarkus.native.container-build=true --define quarkus.container-image.build=true --define quarkus.native.builder-image=quay.io/quarkus/ubi-quarkus-mandrel:21.3-java11
 docker build -f src\main\docker\Dockerfile.jvm -t sprugit/rest-service .
 CD %previous%
